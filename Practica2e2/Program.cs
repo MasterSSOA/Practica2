faf897c9fe2fba0814918eaaf47cd98014da2747
MasterSSOA <tiburon.grande77@gmail.com>
2020-09-01 11:15:04 -0400

    Iniciando la Practica2e2

--- Practica2e2/Program.cs
+++ Practica2e2/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Practica2e2
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-        }
+        // Practica 2 Ejercicio 2. 
+        // Nota : Programa capaz de evaluar edad del usuario y clasificarlo
+
+        //  EJEMPLO:
+        //  Niños(0 – 10)
+        //  Adolescentes(11 – 17)
+        //  Adulto joven(18 – 38)
+        //  Mediana Edad(39 - 59)
+        //  Adulto mayor(60 – 99)
+
+        int age;
+        Console.WriteLine("Digite su edad");
+        Console.ReadKey();
     }
 }
