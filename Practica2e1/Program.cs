820a120f9bf377970731bbe2250862e32cfb5125
Anthony Reyes <AReyes@credigas.local>
2020-09-01 10:43:23 -0400

    Add project files.

---
+++ Practica2e1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Practica 2 Ejercicio 1. 
+        // Nota : Programa capaz de evaluar un numero y decir si es par o no.
+
+        int Value;
+        string Validation = "";
+
+        while (!int.TryParse(Validation, out Value))
+        {
+            Console.WriteLine("Digite un Número: ");
+            Validation = Console.ReadLine();
+
+            if (int.TryParse(Validation, out Value))
+            {
+                Value = Convert.ToInt32(Validation);
+                if (Value % 2 == 0)
+                {
+                    Console.WriteLine("El número {0} es par.", Value);
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("El número {0} es impar.", Value);
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Digite un caracter de tipo númerico!\n");
+
+            }
+        }
+    }
+}
